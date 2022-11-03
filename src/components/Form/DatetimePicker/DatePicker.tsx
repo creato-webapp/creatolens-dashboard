1d5e04b4b022f4c5cad5bded9dd586a6fb46bcd8
kalong-leung <leonellus0123@gmail.com>
2022-11-03 10:34:20 +0800

    TimePicker

---
+++ src/components/Form/DatetimePicker/DatePicker.tsx
@@ -0,0 +1,26 @@
+import type { FC } from 'react'
+import { DatePickerProps } from './interface'
+
+const DatePicker: FC<DatePickerProps> = (props: DatePickerProps) => {
+  return (
+    <div className="mb-4 flex items-center">
+      <input
+        name={props.id}
+        defaultValue={props.defaultValue}
+        id={props.id}
+        min="2000-01-01"
+        type="date"
+        className="
+        mt-1
+        block
+        w-full
+        rounded-md
+        border-gray-300
+        shadow-sm
+        focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+        "
+      />
+    </div>
+  )
+}
+export default DatePicker
---
+++ src/components/Form/DatetimePicker/DateTimePicker.tsx
@@ -0,0 +1,17 @@
+import type { FC } from 'react'
+import { TimePickerProps } from './interface'
+
+const DateTimePicker: FC<TimePickerProps> = (props: TimePickerProps) => {
+  return (
+    <div className="mb-4 flex items-center">
+      <input
+        id={props.id}
+        name={props.id}
+        type="datetime-local"
+        defaultValue={props.defaultValue}
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+      />
+    </div>
+  )
+}
+export default DateTimePicker
