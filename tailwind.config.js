56a0a04fb56d0826f14ef8e310fc89806e41a082
kalong-leung <leonellus0123@gmail.com>
2022-11-07 17:52:24 +0800

    Fix config setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: 'media',
   // ...
-  content: ['./pages/*.{js,ts,jsx,tsx}', './src/components/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
