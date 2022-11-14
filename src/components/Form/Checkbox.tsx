b5d49315070e19201e9f7b7c707c23fe44398b14
kalong-leung <leonellus0123@gmail.com>
2022-11-14 14:19:51 +0800

    checkbox

--- src/components/Form/Checkbox.tsx
+++ src/components/Form/Checkbox.tsx
@@ -3,15 +3,13 @@ import { CheckBoxProps } from './interface'
 
 const Checkbox: FC<CheckBoxProps> = (props: CheckBoxProps) => {
   return (
-    <div className="mb-4 flex items-center">
-      <input
-        id={props.id}
-        name={props.id}
-        type="checkbox"
-        defaultChecked={props.defaultChecked}
-        className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
-      />
-    </div>
+    <input
+      id={props.id}
+      name={props.id}
+      type="checkbox"
+      defaultChecked={props.defaultChecked}
+      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+    />
   )
 }
 export default Checkbox
