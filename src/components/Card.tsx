aa9f51b2db66959fff2dbca6c3f409229201f1b3
kalong-leung <leonellus0123@gmail.com>
2022-11-21 19:08:06 +0800

    Added Extra field in Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,18 +2,23 @@ import { title } from 'process'
 import React from 'react'
 
 interface CardProps {
-  title: string
+  title: React.ReactNode
   children: React.ReactNode
+  extra?: React.ReactNode
 }
 
 export default function Card(props: CardProps) {
-  const { title, children } = props
+  const { title, children, extra } = props
   return (
-    <div className="min-h-72 bg-white shadow-md border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 m-8">
-      <div className="p-5 min-w-full min-h-full ">
-        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-          {title}
-        </h5>
+    <div className="min-h-72 m-8 rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
+      <div className="min-h-full min-w-full p-5 ">
+        <div className="flex">
+          <h5 className=" mb-2 flex-auto text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+          <div className="flex-none">{extra}</div>
+        </div>
+
         {children}
       </div>
     </div>
