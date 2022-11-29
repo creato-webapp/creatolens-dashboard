fec3b5b74982567180aae56db2f1c419156c0ed1
kalong-leung <leonellus0123@gmail.com>
2022-11-29 17:53:44 +0800

    Refractor Layout Component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,45 +3,40 @@ import { Popover } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/24/outline'
 import { useSession, signIn, signOut, getSession } from 'next-auth/react'
 import Link from 'next/link'
-import { User } from 'next-auth'
 
 interface NavbarProps {
   children: React.ReactNode
 }
 
 export default function Navbar(props: NavbarProps) {
-  const { data: session, status } = useSession()
-  const user = session?.user as User
-
   type navBarItem = {
     title: string
     href: string
-    role: [string | null]
   }
 
   const navBarItemList: Array<navBarItem> = [
-    { title: 'Home', href: '/', role: ['player'] },
-    { title: 'Accounts', href: '/accounts', role: ['player'] },
-    { title: 'Blocked Accounts', href: '/accounts-blocked', role: ['player'] },
-    { title: 'Retry Accounts', href: '/accounts-retry', role: ['player'] },
-    { title: 'Account Login Error', href: '/accounts-error', role: ['player'] },
+    { title: 'Home', href: '/' },
+    { title: 'Accounts', href: '/accounts' },
+    { title: 'Blocked Accounts', href: '/accounts-blocked' },
+    { title: 'Retry Accounts', href: '/accounts-retry' },
+    { title: 'Account Login Error', href: '/accounts-error' },
   ]
 
-  return session ? (
+  const { data: session, status } = useSession()
+
+  return (
     <>
       <Popover className="relative bg-white">
         <div className="mx-auto max-w-7xl px-4 sm:px-6">
           <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <Popover.Group as="nav" className="hidden space-x-8 md:flex">
-              {navBarItemList.map((e, index) =>
-                e.role.includes(user?.role) ? (
-                  <Link href={e.href} key={index} replace>
-                    <a className="text-base font-medium text-gray-500 hover:text-gray-900">
-                      {e.title}
-                    </a>
-                  </Link>
-                ) : null
-              )}
+              {navBarItemList.map((e, index) => (
+                <Link href={e.href} key={index} replace>
+                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    {e.title}
+                  </a>
+                </Link>
+              ))}
             </Popover.Group>
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
               {session ? (
@@ -71,5 +66,5 @@ export default function Navbar(props: NavbarProps) {
       </Popover>
       {props.children}
     </>
-  ) : null
+  )
 }
