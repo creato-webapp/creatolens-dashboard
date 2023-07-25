e37a2addfc7d84639e06311dd35be9755a852d2d
kalong-leung <leonellus0123@gmail.com>
2023-07-25 18:10:39 +0800

    Added Radio Component

--- src/components/Form/Radio/Radio.tsx
+++ src/components/Form/Radio/Radio.tsx
@@ -4,18 +4,19 @@ import React, { ChangeEvent, useState } from 'react'
 
 // Define the props for the Radio component
 interface RadioProps {
-  value: string
-  checked: boolean
-  label: string
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  checked?: boolean
+  label?: string
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+  className?: string
 }
 
 // Radio component
-export const Radio: React.FC<RadioProps> = ({ value, checked, label, onChange }) => {
+export const Radio: React.FC<RadioProps> = ({ value, checked, label, onChange, className }) => {
   return (
     <label className="radio-label mx-2 flex flex-row items-center text-text-primary">
       <input
-        className="form-radio mx-2 text-orange-500 focus:ring-2 focus:ring-orange-500"
+        className={`form-radio mx-2 text-orange-500 focus:ring-2 focus:ring-orange-500 ${className}`}
         type="radio"
         value={value}
         checked={checked}
