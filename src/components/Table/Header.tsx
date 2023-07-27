4639442bbd7c06ea445617606138b97c9be409e3
kalong-leung <leonellus0123@gmail.com>
2023-07-27 12:29:04 +0800

    Refine Table components

--- src/components/Table/Header.tsx
+++ src/components/Table/Header.tsx
@@ -2,14 +2,10 @@ import { HeaderProps } from './Interface'
 
 const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
   return (
-    <thead className="bg-gray-50 text-xs uppercase text-gray-700">
+    <thead className="bg-neutral-100 uppercase text-gray-700">
       <tr>
         {props.columns.map((e, index) => (
-          <th
-            scope="col"
-            className="border border-slate-300 py-2 px-4"
-            key={index}
-          >
+          <th scope="col" className="border border-slate-300 py-2 px-4" key={index}>
             <div className="flex items-center">{e.title}</div>
             {props.children}
           </th>
--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -2,12 +2,9 @@ import { RowProps } from './Interface'
 
 const Row: React.FC<RowProps> = (props: RowProps) => {
   return (
-    <tr
-      key={props.key}
-      className=" border-b dark:border-gray-700 dark:bg-gray-800"
-    >
+    <tr key={props.key} className="border-b">
       {props.columns.map(({ title, dataIndex, render }, index) => (
-        <td key={index} className="break-all border border-slate-300 py-2 px-4">
+        <td key={index} className="min-w-32 h-12 items-center justify-start border border-slate-300 bg-neutral-50 p-2 ">
           {render ? render(props.rowData[dataIndex]) : props.rowData[dataIndex]}
         </td>
       ))}
