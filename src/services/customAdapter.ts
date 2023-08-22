5af31da9fca422162463c6c93aeb6a5518547bc8
kalong-leung <leonellus0123@gmail.com>
2023-08-22 16:11:29 +0800

    service and adaptor type

--- src/services/customAdapter.ts
+++ src/services/customAdapter.ts
@@ -1,5 +1,6 @@
 import { FirestoreAdapter } from '@next-auth/firebase-adapter'
 import { getRoles } from '@services/util'
+import { Awaitable } from 'next-auth'
 import { AdapterUser } from 'next-auth/adapters'
 export interface FirestoreAdapterOptions {
   emulator?: {
@@ -18,7 +19,7 @@ export const FireStoreAdapterWrapper: typeof FirestoreAdapter = (options) => {
     const roles = await getRoles(user.email)
     console.log({ roles })
     const combinedUser = { ...(user as CombinedUser), roles: roles }
-    return FirestoreAdapter(options).createUser(combinedUser)
+    return adapter.createUser?.(combinedUser) as Awaitable<AdapterUser>
   }
   return adapter
 }
