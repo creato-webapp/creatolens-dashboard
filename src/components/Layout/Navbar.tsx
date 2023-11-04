3ad380507fe0130ba7dcfbe85aa3e37c59fe4a44
kalong-leung <leonellus0123@gmail.com>
2023-11-04 18:21:43 -0400

    Nav Bar Responsive

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { Button } from '..'
 import { Title } from '@components/Typography'
 import { useRouter } from 'next/router'
+import MenuIcon from '@components/Icon/MenuIcon'
+import LogoutIcon from '@components/Icon/LogOutIcon'
+import CrossIcon from '@components/Icon/CrossIcon'
+
 interface Page {
   name: string
   path: string
@@ -13,42 +17,46 @@ interface NavBarProps {
   isLoggedIn: boolean
   onLogin: () => void
   onLogout: () => void
+  isCollapse?: boolean
 }
 
-const NavBar: React.FC<NavBarProps> = ({ logo, pages, isLoggedIn, onLogin, onLogout }) => {
+const NavBar: React.FC<NavBarProps> = ({ logo, pages, isLoggedIn, onLogin, onLogout, isCollapse = true }) => {
   const [isLoading, setIsLoading] = useState(false)
+  const [isMenuCollapse, setIsMenuCollapse] = useState(isCollapse)
   const router = useRouter()
-  return (
-    <nav className="relative z-50 flex h-1/4 items-center justify-between bg-bg-dark">
-      <div className="ml-6">
-        <a href="/">
-          <img src={logo} alt="Logo" className="h-24 w-auto" />
-        </a>
-      </div>
 
-      <div className="flex space-x-10 justify-self-center">
+  const toggleMenu = () => {
+    setIsMenuCollapse((prev) => !prev)
+  }
+  return (
+    <nav className="relative z-50 flex h-auto justify-between bg-bg-dark">
+      <div className="mx-8 my-auto md:hidden">
+        <Button.Text className="text-text-primary" onClick={toggleMenu}>
+          <MenuIcon></MenuIcon>
+        </Button.Text>
+      </div>
+      <a href="/" className="mx-8 my-auto shrink-0 md:mx-20">
+        <img src={logo} alt="Logo" className="h-16" />
+      </a>
+      <div className="hidden space-x-10 justify-self-center md:flex">
         {pages.map((page, index) => (
-          <div
-            id={`${page.name}-${index}-div-1`}
-            key={`${page.name}-${index}`}
-            className="inline-flex flex-col items-center justify-start gap-8 pb-9"
-          >
-            <div
-              key={`${page.name}-${index}-div-2`}
-              className={`${router.pathname === page.path ? 'h-1 self-stretch bg-orange-500' : 'h-1 self-stretch bg-red-500 opacity-0'}`}
-            />
-            <a key={`${page.name}-${index}-a`} href={page.path}>
-              <Title key={`${page.name}-${index}-title`} bold level={3} className={` ${router.pathname === page.path ? ' text-orange-500' : ''}`}>
+          <div key={`${page.name}-${index}`} className="inline-flex flex-col items-center justify-start gap-8 pb-9">
+            <div className={`${router.pathname === page.path ? 'h-1 self-stretch bg-orange-500' : 'h-1 self-stretch bg-red-500 opacity-0'}`} />
+            <a href={page.path}>
+              <Title bold level={3} className={`${router.pathname === page.path ? ' text-orange-500' : ''}`}>
                 {page.name}
               </Title>
             </a>
           </div>
         ))}
       </div>
-
-      <div className="m-6">
+      <div className="m-8 md:hidden">
+        <LogoutIcon></LogoutIcon>
+      </div>
+      <div className="m-6 my-auto hidden md:flex">
         {isLoggedIn ? (
-          <Button.Text loading={isLoading} onClick={onLogout} className="rounded">
+          <Button.Text loading={isLoading} onClick={onLogout} className="flex rounded">
+            <LogoutIcon className="mr-1"></LogoutIcon>
             <Title level={3} bold>
               Logout
             </Title>
@@ -61,6 +69,30 @@ const NavBar: React.FC<NavBarProps> = ({ logo, pages, isLoggedIn, onLogin, onLog
           </Button.Text>
         )}
       </div>
+      <aside
+        id="default-sidebar"
+        className={`fixed z-40 h-screen w-screen -translate-x-full transition-transform sm:translate-x-0 ${isMenuCollapse ? 'hidden' : 'block'}`}
+        aria-label="Sidebar"
+      >
+        <div className="flex h-full flex-col overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <Button.Text className="m-8 ml-auto text-text-primary" onClick={() => setIsMenuCollapse(true)}>
+            <CrossIcon className=""></CrossIcon>
+          </Button.Text>
+
+          <ul className="mx-auto flex-row space-y-16">
+            {pages.map((page, index) => (
+              <li key={`${page.name}-${index}`} className="text-center">
+                <div className={`${router.pathname === page.path ? 'h-1 self-stretch bg-orange-500' : 'h-1 self-stretch bg-red-500 opacity-0'}`} />
+                <a href={page.path}>
+                  <Title bold level={3} className={`${router.pathname === page.path ? ' text-orange-500' : ''}`}>
+                    {page.name}
+                  </Title>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
     </nav>
   )
 }
