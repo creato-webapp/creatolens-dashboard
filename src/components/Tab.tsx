989980ca67164cacc56e09c6472b0d74dd70d4ce
kalong-leung <leonellus0123@gmail.com>
2023-11-20 11:58:13 -0500

    updated tab component

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 
-// Define the TabItem type
-export interface TabItemType {
+export interface TabItem {
   key: string
   title: string
-  content: React.ReactNode
+  children: React.ReactNode
 }
 
 interface TabProps extends React.HTMLAttributes<HTMLDivElement> {
   activeKey?: string
   centered?: boolean
   defaultActiveKey?: string
-  items: TabItemType[]
+  items: TabItem[]
   size?: 'large' | 'middle' | 'small'
   scrollable?: boolean
   tabBarExtraContent?: React.ReactNode
@@ -50,19 +49,18 @@ const Tab: React.FC<TabProps> = ({
   }
 
   return (
-    <div className={`tabs inline-block min-h-0 min-w-full p-4 ${className ? className : ''}`}>
-      <div className={`tabs-bar${centered ? ' centered' : ''}  inline-flex`} style={tabBarStyle}>
+    <div className={`tabs inline-block h-auto w-auto min-w-full ${className ? className : ''}`}>
+      <div className={`tabs-bar${centered ? ' centered' : ''} inline-flex `} style={tabBarStyle}>
         {items.map((item) => (
           <button
             type="button"
-            className={`px-6 py-2 ${currentActiveKey == item.key ? 'bg-accent1-500 text-text-white' : 'bg-bg-white text-text-primary'}`}
+            className={`disabled:text-text-disable h-[2.125rem] w-auto min-w-[12.5rem] hover:bg-accent1-300 hover:text-text-white focus:bg-accent1-500 active:bg-accent1-500 disabled:bg-disabled md:h-[3.375rem] ${
+              currentActiveKey == item.key ? 'bg-accent1-500 text-text-white' : 'bg-bg-white text-text-primary'
+            }`}
+            onClick={() => handleChange(item.key)}
           >
-            <div
-              key={item.key}
-              className={`tab${currentActiveKey === item.key ? ' active' : ''} ${size || ''} p-2 `}
-              onClick={() => handleChange(item.key)}
-            >
-              {item.title}
+            <div key={item.key} className={`tab${currentActiveKey === item.key ? ' active' : ''} ${size || ''} d flex items-center justify-center `}>
+              <h4>{item.title}</h4>
             </div>
           </button>
         ))}
@@ -75,7 +73,7 @@ const Tab: React.FC<TabProps> = ({
               key={item.key}
               className={`tab-pane${currentActiveKey === item.key ? ' active' : ''}  inline-block min-w-full flex-col justify-center`}
             >
-              {item.content}
+              {item.children}
             </div>
           ) : null
         )}
