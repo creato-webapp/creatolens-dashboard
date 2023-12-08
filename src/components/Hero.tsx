bae023bc97de83969f54d1c35f35be0f28838dc4
Timothy Lam <timothy.lam@creatogether.app>
2023-12-07 19:22:41 -0500

    Remove desktop nav bar bottom border

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,10 +8,7 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ backgroundImage, children, className }: HeroProps) => {
   return (
-    <div
-      className={`relative bg-cover bg-center bg-no-repeat md:border-2 md:border-b-accent1-500 md:pb-0 ${className}`}
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
+    <div className={`relative bg-cover bg-center bg-no-repeat md:pb-0 ${className}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="bg-white md:bg-black md:bg-opacity-50 md:backdrop-blur-sm md:backdrop-filter">
         <div className="mb-0 p-4 pl-10 text-left text-text-primary md:p-8 md:py-8 md:text-white">{children}</div>
       </div>
