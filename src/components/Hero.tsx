12daba56e17ca1e69a90792a04b3fdc92c10e0d9
Timothy Lam <timothy.lam@creatogether.app>
2023-12-10 16:58:31 -0500

    add stroke line & fix height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,16 +4,14 @@ interface HeroProps {
   backgroundImage: string
   children: ReactNode
   className?: string
+  childrenStyle?: string
 }
 
-const Hero: React.FC<HeroProps> = ({ backgroundImage, children, className }: HeroProps) => {
+const Hero: React.FC<HeroProps> = ({ backgroundImage, children, className, childrenStyle }: HeroProps) => {
   return (
-    <div
-      className={`relative bg-cover bg-center bg-no-repeat md:border-2 md:border-b-accent1-500 md:pb-0 ${className}`}
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="bg-white md:bg-black md:bg-opacity-50 md:backdrop-blur-sm md:backdrop-filter">
-        <div className="mb-0 p-4 pl-10 text-left text-text-primary md:p-8 md:py-8 md:text-white">{children}</div>
+    <div className={`relative bg-cover bg-center bg-no-repeat md:pb-0 ${className}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="bg-white md:bg-black md:bg-opacity-50 md:backdrop-filter h-full">
+        <div className={`mb-0 px-4 py-10 text-left text-text-primary md:p-8 md:py-16 md:pl-10 md:text-white ${childrenStyle}`}>{children}</div>
       </div>
     </div>
   )
