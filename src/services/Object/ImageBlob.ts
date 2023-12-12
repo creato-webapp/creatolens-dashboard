308d299a9d269e649f95ec1fd22d033008146a9d
kalong-leung <leonellus0123@gmail.com>
2023-12-12 18:57:59 -0500

    test page for 3 labels

--- src/services/Object/ImageBlob.ts
+++ src/services/Object/ImageBlob.ts
@@ -2,6 +2,11 @@ import { AxiosRequestConfig } from 'axios'
 import { Fetcher } from '../fetcher'
 import { imageToBase64 } from '../util'
 
+type hashtag = {
+  acc: number
+  hashtag: string
+}
+
 export type Labels = {
   description: string
   score: number
@@ -10,6 +15,10 @@ export type Labels = {
 export type ImageResponse = {
   labels: Array<Labels>
   data: Array<ImageRecord>
+  firstTwo: Array<hashtag>
+  middleTwo: Array<hashtag>
+  lastTwo: Array<hashtag>
+  error?: any
 }
 
 export type ImageRecord = {
@@ -17,8 +26,20 @@ export type ImageRecord = {
   target: Array<string>
 }
 
+function validateImage(file: File, maxSize: number, validTypes: string[]): void {
+  if (file.size > maxSize) {
+    window.alert(`File size should not exceed ${maxSize / 1024 / 1024} MB`)
+  }
+
+  if (!validTypes.includes(file.type)) {
+    window.alert(`Invalid file type. The following types are supported: ${validTypes.join(', ')}`)
+  }
+}
+
 export async function uploadImage(file: File, customConfig?: AxiosRequestConfig): Promise<ImageResponse> {
   try {
+    validateImage(file, 2 * 1024 * 1024, ['image/jpeg', 'image/png', 'image/gif'])
+
     const imageString = await imageToBase64(file)
     const response = await Fetcher.POST(
       '/api/blob',
