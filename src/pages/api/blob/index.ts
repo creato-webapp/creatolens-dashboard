8c569a8c45030fbc2a0a480d00cb92881e5c25fc
kalong-leung <leonellus0123@gmail.com>
2023-12-12 19:21:50 -0500

    add await

--- src/pages/api/blob/index.ts
+++ src/pages/api/blob/index.ts
@@ -31,56 +31,60 @@ export default async function accountQueryHandler(req: NextApiRequest, res: Next
     //   return res.status(response.status).json(response.data)
     // }
 
-    case 'POST': {
-      try {
-        if (!req.headers.cookie) {
-          return res.status(401).json({ message: 'Unauthorized' })
-        }
-        if (!req.body) {
-          return res.status(400).json({ message: 'Invalid request' })
-        }
+    case 'POST':
+      {
+        try {
+          if (!req.headers.cookie) {
+            return res.status(401).json({ message: 'Unauthorized' })
+          }
+          if (!req.body) {
+            return res.status(400).json({ message: 'Invalid request' })
+          }
 
-        const response = await BlobInstance.post('/cloud-vision', body, {
-          headers: {
-            Cookie: req.headers.cookie,
-          },
-        })
-        console.log(response)
-        if (response.status !== 200) {
-          return res.status(response.status).json({ message: 'Something went wrong' })
-        }
-        if (!response.data) {
-          return res.status(400).json({ message: 'Invalid request' })
-        }
-        const labels = response.data.map((e: any) => e.description)
-        console.log(labels.join(', '))
-        const hashtagRes = Promise.allSettled([
-          axios.get(process.env.IMAGE_HASHTAG_1 as string, {
-            params: { input: labels.join(', ') },
-          }),
-          axios.get(process.env.IMAGE_HASHTAG_2 as string, {
-            params: { input: labels.join(', ') },
-          }),
-          axios.get(process.env.IMAGE_HASHTAG_3 as string, {
-            params: { input: labels.join(', ') },
-          }),
-        ])
-          .then((results) => {
-            const data1 = results[0].status === 'fulfilled' ? results[0].value.data.data : null
-            const data2 = results[1].status === 'fulfilled' ? results[1].value.data.data : null
-            const data3 = results[2].status === 'fulfilled' ? results[2].value.data.data : null
-
-            const error = results.find((result) => result.status === 'rejected')?.status || null
-
-            return res.status(response.status).json({ labels: response.data, firstTwo: data1, middleTwo: data2, lastTwo: data3, error: error })
+          const response = await BlobInstance.post('/cloud-vision', body, {
+            headers: {
+              Cookie: req.headers.cookie,
+            },
           })
-          .catch((error) => {
-            console.log(error)
-            return res.status(response.status).json({ labels: response.data, error: error })
-          })
-        return hashtagRes
-      } catch (error) {}
-    }
+          console.log(response)
+          if (response.status !== 200) {
+            return res.status(response.status).json({ message: 'Something went wrong' })
+          }
+          if (!response.data) {
+            return res.status(400).json({ message: 'Invalid request' })
+          }
+          const labels = response.data.map((e: any) => e.description)
+          console.log(labels.join(', '))
+          const hashtagRes = await Promise.allSettled([
+            axios.get(process.env.IMAGE_HASHTAG_1 as string, {
+              params: { input: labels.join(', ') },
+            }),
+            axios.get(process.env.IMAGE_HASHTAG_2 as string, {
+              params: { input: labels.join(', ') },
+            }),
+            axios.get(process.env.IMAGE_HASHTAG_3 as string, {
+              params: { input: labels.join(', ') },
+            }),
+          ])
+            .then((results) => {
+              const data1 = results[0].status === 'fulfilled' ? results[0].value.data.data : null
+              const data2 = results[1].status === 'fulfilled' ? results[1].value.data.data : null
+              const data3 = results[2].status === 'fulfilled' ? results[2].value.data.data : null
+
+              const error = results.find((result) => result.status === 'rejected')?.status || null
+
+              return res.status(response.status).json({ labels: response.data, firstTwo: data1, middleTwo: data2, lastTwo: data3, error: error })
+            })
+            .catch((error) => {
+              console.log(error)
+              return res.status(response.status).json({ labels: response.data, error: error })
+            })
+          return hashtagRes
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      return res.status(400).json({ message: 'Nothing happened' })
     default:
       res.setHeader('Allow', ['GET', 'POST'])
       res.status(405).end(`Method ${method} Not Allowed`)
