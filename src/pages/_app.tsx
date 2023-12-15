978bf20755726d89ca0391d0796623d2b830c122
Timothy.Lam <timothy.lam@creatogether.app>
2023-12-14 20:57:24 -0500

    change logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,9 @@ function MyApp({
 }>) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Head>{/* <link key="icon" rel="icon" href="./favicon.ico" /> */}</Head>
+      <Head>
+        <link key="icon" rel="icon" href="./favicon.ico" />
+      </Head>
       <Navbar>
         <Component {...pageProps} />
       </Navbar>
