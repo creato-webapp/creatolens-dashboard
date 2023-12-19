dbd65344d40ba41e39c96bb35f3ba3ed79041194
Timothy.Lam <timothy.lam@creatogether.app>
2023-12-19 10:04:39 -0500

    add a tailwindcss class generator

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,18 +1,18 @@
 export const getClasses = (index: number, classes: string) => {
-    if (index === 0) {
-      return classes
-    } else if (index === 1) {
-      const mdClasses = classes
-        .split(' ')
-        .map((c) => `md:${c}`)
-        .join(' ')
-      return `${mdClasses}`
-    } else if (index === 2) {
-      const lgClasses = classes
-        .split(' ')
-        .map((c) => `lg:${c}`)
-        .join(' ')
-      return lgClasses
-    }
-    return index === 0 ? classes : `md:${classes} lg:${classes}`
+  if (index === 0) {
+    return classes
+  } else if (index === 1) {
+    const mdClasses = classes
+      .split(' ')
+      .map((c) => ` md:${c}`)
+      .join(' ')
+    return `${mdClasses}`
+  } else if (index === 2) {
+    const lgClasses = classes
+      .split(' ')
+      .map((c) => ` lg:${c}`)
+      .join(' ')
+    return lgClasses
   }
+  return index === 0 ? classes : ` md:${classes} lg:${classes}`
+}
