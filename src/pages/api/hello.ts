c8e5df8d8c51c0ac3154dbc854c58721d8d1ddce
kalong-leung <leonellus0123@gmail.com>
2024-03-09 04:40:08 -0500

    Add request-ip package and update dependencies

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import requestIp from 'request-ip'
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const detectedIp = requestIp.getClientIp(req)
+  console.log(detectedIp)
   res.status(200).json({ name: 'John Doe' })
 }
