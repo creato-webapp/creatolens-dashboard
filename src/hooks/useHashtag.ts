3ce106be27f742c0a74fe9451629a14cbc922740
kalong-leung <leonellus0123@gmail.com>
2024-03-25 16:29:31 -0400

    lower case letter fuction name

--- src/hooks/useHashtag.ts
+++ src/hooks/useHashtag.ts
@@ -1,8 +1,8 @@
 import useSWR from 'swr'
-import { GetHashtag } from '@services/HashtagHelper'
+import { getHashtag } from '@services/HashtagHelper'
 import { IHashet } from 'src/pages/recommendation'
 export const useGetHashtag = (input: string, shouldFetch: boolean = true, fallbackData?: { data: IHashet[] }) => {
-  const { data, error, mutate, ...swr } = useSWR(shouldFetch ? input : null, GetHashtag, {
+  const { data, error, mutate, ...swr } = useSWR(shouldFetch ? input : null, getHashtag, {
     refreshInterval: 0,
     fallbackData: fallbackData,
     revalidateOnFocus: false,
