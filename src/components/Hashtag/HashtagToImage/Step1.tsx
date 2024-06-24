defc265d451a66e7b9f25790b605a3771df5284b
timothylam <timothylam1228@gmail.com>
2024-06-24 14:38:04 -0400

    update Hashtag to image

--- src/components/Hashtag/HashtagToImage/Step1.tsx
+++ src/components/Hashtag/HashtagToImage/Step1.tsx
@@ -5,8 +5,27 @@ export interface StepProps {
   setStep: (arg: number) => void
 }
 const Step1 = (props: StepProps) => {
+  const { setStep } = props
+
+  const gotoNextStep = () => {
+    setStep(2)
+    return null
+  }
   return (
     <>
+      <h2 className="font-extrabold">Keywords input</h2>
+      <div className="mt-4 flex items-center justify-center">
+        <Outline sizes={['full', 'l', 'l']}>+ Get Keywords from Image</Outline>
+      </div>
+      <div className="my-4 border-b"></div>
+      <h3 className="font-semibold text-text-primary">Describe the image you want and we’ll generate image for you.</h3>
+      <textarea className="mt-4 min-h-96 w-full border border-black p-5 text-text-disabled" placeholder="Input your own keyword" />
+
+      <div className="mt-4 flex items-center justify-center">
+        <Primary onClick={gotoNextStep} sizes={['full', 'full', 'full']}>
+          Next
+        </Primary>
+      </div>
     </>
   )
 }
