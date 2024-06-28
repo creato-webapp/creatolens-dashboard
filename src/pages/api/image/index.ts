f0c3b3792f6cdfb2aa2ab385fc09b914e0bd858c
kalong-leung <leonellus0123@gmail.com>
2024-06-28 02:24:26 -0400

    changed image upload logic with fs

--- src/pages/api/image/index.ts
+++ src/pages/api/image/index.ts
@@ -1,6 +1,27 @@
+import FormData from "form-data";
+import formidable from 'formidable'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import fs from 'fs'
+
 import ImageInstance from '../axiosInstance/Image'
+
+export const config = {
+  api: {
+    bodyParser: false, // Disable the default body parser
+  },
+}
+
+
+const parseForm = (req: NextApiRequest): Promise<{ fields: formidable.Fields, files: formidable.Files }> => {
+  return new Promise((resolve, reject) => {
+    const form = formidable({ multiples: true })
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err)
+      resolve({ fields, files })
+    })
+  })
+}
 
 
 export default async function ImageUpload(req: NextApiRequest, res: NextApiResponse) {
@@ -9,9 +30,23 @@ export default async function ImageUpload(req: NextApiRequest, res: NextApiRespo
   switch (method) {
     case 'POST': {
       try {
-        const response = await ImageInstance.post(`/api/image-tagen`, req.body, {
+
+        const { fields, files } = await parseForm(req)
+
+        const fileArray = files.file as formidable.File[] 
+        const usernameArray = fields.username as string[]
+
+        const file = fileArray[0] as formidable.File
+        const fileStream = fs.createReadStream(file.filepath)
+    
+        const formData = new FormData()
+
+        formData.append('file', fileStream, file.originalFilename as string)
+        formData.append('username', usernameArray[0] as string)
+        
+        const response = await ImageInstance.post(`/api/image-tagen`, formData, {
           headers: {
-            'Content-Type': req.headers['content-type'],
+            'Content-Type': 'multipart/form-data',
           },
         })
         return res.status(response.status).json(response.data)
