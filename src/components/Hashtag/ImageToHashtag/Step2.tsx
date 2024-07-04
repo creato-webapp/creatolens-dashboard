2e847a106c23272a27d1f80f92ecf57386e485f3
timothylam <timothylam1228@gmail.com>
2024-07-04 18:42:56 -0400

    minor update

--- src/components/Hashtag/ImageToHashtag/Step2.tsx
+++ src/components/Hashtag/ImageToHashtag/Step2.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import Image from 'next/image'
 import Skeleton from 'react-loading-skeleton'
 
 import { Badges } from '@components/Badges'
 import Outline from '@components/Button/Outline'
+import OutlinePrimaryButton from '@components/Button/OutlinePrimary'
 import Primary from '@components/Button/Primary'
 import { useImageHashtagContext } from 'src/context/ImageToHashtagContext'
 
@@ -13,8 +14,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const Step2 = (props: StepProps) => {
   const { setStep } = props
-  const { images, currentImageIndex, getCurrentImageLabels, updateSelectedLabels, loadingLabels } = useImageHashtagContext()
-  const [reAnnotateTimes, setReAnnotateTimes] = useState<number>(1)
+  const { images, currentImageIndex, getCurrentImageLabels, updateSelectedLabels, selectAllLabels, loadingLabels } = useImageHashtagContext()
 
   useEffect(() => {
     const currentImageObj = images[currentImageIndex]
@@ -32,19 +32,32 @@ const Step2 = (props: StepProps) => {
   }
 
   const onReannotateClick = () => {
-    setReAnnotateTimes((pre) => pre + 1)
     getCurrentImageLabels()
+  }
+
+  const onGoBack = () => {
+    setStep(1)
   }
   const onClickButton = () => {
     setStep(3)
   }
 
+  const onClickSelectAll = () => {
+    selectAllLabels()
+  }
+
   const currentImage = useMemo(() => {
     return images[currentImageIndex]
-  }, [images, currentImageIndex])
+  }, [images, currentImageIndex, images[currentImageIndex].selectedLabels])
 
   return (
     <div className="flex w-full flex-col gap-4 md:flex-row">
+      <div className="flex flex-row">
+        <div className="w-fit items-center justify-center px-4 text-center text-2xl text-black" onClick={onGoBack}>
+          {'<'}
+        </div>
+        <h2 className="flex items-center font-extrabold">Image label annotation</h2>
+      </div>
       <div className="relative my-4 flex h-48 w-full items-center justify-center md:w-1/2">
         {currentImage.image && (
           <Image
@@ -57,11 +70,9 @@ const Step2 = (props: StepProps) => {
           />
         )}
       </div>
+      <div className="my-4 border-b"></div>
       <div className="flex w-full flex-col gap-2 md:w-1/2">
-        <h2 className="font-extrabold">Image label annotation</h2>
-        {currentImage.labels && (
-          <h3 className="font-semibold">Selected labels: {`${currentImage.selectedLabels.length} / ${currentImage.labels?.length}`}:</h3>
-        )}
+        {currentImage.labels && <h3 className="mb-4 font-semibold text-text-secondary">{`${currentImage.labels?.length}`} labels discovered</h3>}
         <div>
           <div className="flex h-full w-full flex-row flex-wrap gap-4">
             {!loadingLabels ? (
@@ -71,11 +82,12 @@ const Step2 = (props: StepProps) => {
                   <Badges
                     key={`key-${label}`}
                     rounded
-                    closeable
-                    isOutline={!currentImage.selectedLabels.includes(label)}
+                    // isDisabled={currentImage.selectedLabels.includes(label)}
+                    isOutline
+                    // isOutline
+                    status={!currentImage.selectedLabels.includes(label) ? 'text-secondary' : 'primary'}
                     onClose={() => onClose(label)}
                     onClick={() => onSelected(label)}
-                    status={'primary'}
                   >
                     {label}
                   </Badges>
@@ -87,10 +99,13 @@ const Step2 = (props: StepProps) => {
           </div>
         </div>
         <div>
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
+            <OutlinePrimaryButton sizes={['l', 'l', 'l']} onClick={onClickSelectAll}>
+              Select All
+            </OutlinePrimaryButton>
             <Outline sizes={['l', 'l', 'l']} onClick={onReannotateClick}>
               <Image src="/arrows-clockwise.png" height={24} width={24} alt={'arrows clockwise'} />
-              Re-Annotate({reAnnotateTimes})
+              Re-annotate unselected label
             </Outline>
             <Primary sizes={['l', 'l', 'l']} onClick={onClickButton}>
               + Get Hashtag
