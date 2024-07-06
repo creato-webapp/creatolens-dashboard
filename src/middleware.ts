f971e07c635c0e4e6f6c7eb38044b7c4e73e9ded
kalong-leung <leonellus0123@gmail.com>
2024-07-06 14:26:42 -0400

    Added path in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/accounts/:path', '/recommendation', '/guide'] }
+export const config = { 
+    matcher: [
+      '/accounts/:path*', 
+      '/recommendation', 
+      '/guide',
+      '/api/image/:path*',
+      '/api/hashtag/:path*',
+    ]
+  }
