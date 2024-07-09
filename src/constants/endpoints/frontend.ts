d4f3b66d6f4b81dab26c67230d4409ee20aa57d2
kalong-leung <leonellus0123@gmail.com>
2024-07-08 23:59:50 -0400

    Added Ratelimit and Login to call api logic middleware

--- src/constants/endpoints/frontend.ts
+++ src/constants/endpoints/frontend.ts
@@ -1,5 +1,7 @@
 const ENDPOINT_FRONTEND = {
     CREATE_NEW_ACCOUNT: 'accounts/create',
+
+    ANNOTATE_LABELS: '/api/image/labels',
   } as const
   
   export default ENDPOINT_FRONTEND
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
-export { default } from 'next-auth/middleware'
+import { NextRequest, NextResponse  } from 'next/server'
 
-export const config = { 
-    matcher: [
-      '/accounts/:path*', 
-      '/recommendation', 
-      '/guide',
-      '/api/image/:path*',
-      '/api/hashtag/:path*',
-    ]
+import { authMiddleware } from './middlewares/authMiddleware'
+import rateLimitMiddleware from './middlewares/rateLimitMiddleware'
+
+
+async function runMiddlewares(request: NextRequest, middlewares: CallableFunction[]) {
+  for (const middleware of middlewares) {
+    const response = await middleware(request)
+    if (response) {
+      return response
+    }
   }
+  return NextResponse.next()
+}
+
+export default async function middleware(request: NextRequest) {
+  const middlewares = [authMiddleware, rateLimitMiddleware]
+  return await runMiddlewares(request, middlewares)
+}
+
+export const config = {
+  matcher: ['/dashboard', '/accounts/:path*', '/recommendation', '/guide', '/api/image/:path*', '/api/hashtag/:path*'],
+}
