6be0d80e86fedb2b46f13585fc9e48fe9cb25bd0
kalong-leung <leonellus0123@gmail.com>
2024-07-11 05:11:12 -0400

    Added Remote Config Endpoint

---
+++ src/utils/backend/firebaseAdmin.ts
@@ -0,0 +1,15 @@
+import admin, { ServiceAccount } from 'firebase-admin'
+
+const serviceAccount = {
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+} as ServiceAccount
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+export const remoteConfig = admin.remoteConfig()
+
+module.exports = { remoteConfig }
