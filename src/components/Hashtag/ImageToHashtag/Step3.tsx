ddb893d73a15ad0a86cab4e4d2fa693d518448c3
Timothy.Lam <timothy.lam@creatogether.app>
2024-10-22 12:24:59 -0400

    update tabs style

--- src/components/Hashtag/ImageToHashtag/Step3.tsx
+++ src/components/Hashtag/ImageToHashtag/Step3.tsx
@@ -11,6 +11,7 @@ import router from 'next/router'
 import { CONFIDENCE_LEVELS } from '@constants/imageStyle'
 import { useDialogues } from '@hooks/useDialogues'
 import { Status } from '@context/DialogueContext'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/Tabs'
 
 interface Option {
   label: string
@@ -146,22 +147,35 @@ const Step3: React.FC = () => {
           />
         </div>
       )}
-
-      {labelOptions && (
-        <div className="flex flex-col gap-4">
-          <DropdownCheckbox dropDownSizes={['l', 'l', 'l']} key="label-dropdown" name="Label" options={labelOptions} onValueChange={onClickLabel} />
-        </div>
-      )}
-
-      <div className="my-4 border-b md:hidden" />
-      <h3 className="my-4 text-text-secondary">{`${hashtagsLength} hashtags discovered`}</h3>
-
-      {categorizedOptions.map((option) => (
-        <div key={`${option.name}-dropdown`} className="my-4">
-          <DropdownCheckbox dropDownSizes={['l', 'l', 'l']} name={option.name} options={option.options} onValueChange={onClickHashtag} />
-        </div>
-      ))}
-
+      <Tabs defaultValue="hashtags" className="w-full">
+        <TabsList>
+          <TabsTrigger value="hashtags">Hashtags</TabsTrigger>
+          <TabsTrigger value="labels">Labels</TabsTrigger>
+        </TabsList>
+        <TabsContent value="hashtags">
+          <div className="rounded-lg border border-neutral-300 p-4">
+            <h3 className="my-4 text-text-secondary">{`${hashtagsLength} hashtags discovered`}</h3>
+            {categorizedOptions.map((option) => (
+              <div key={`${option.name}-dropdown`} className="my-4">
+                <DropdownCheckbox dropDownSizes={['l', 'l', 'l']} name={option.name} options={option.options} onValueChange={onClickHashtag} />
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="labels">
+          {labelOptions && (
+            <div className="flex flex-col gap-4">
+              <DropdownCheckbox
+                dropDownSizes={['l', 'l', 'l']}
+                key="label-dropdown"
+                name="Label"
+                options={labelOptions}
+                onValueChange={onClickLabel}
+              />
+            </div>
+          )}
+        </TabsContent>
+      </Tabs>
       <div className="my-4 flex w-full flex-col gap-4">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3">
           <Outline onClick={onClickClearAll} sizes={['s', 'l', 'l']}>
