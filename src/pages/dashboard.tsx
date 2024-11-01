0e7c33e6b189f52bdbd1a8ee8ac28af4faff7086
Timothy.Lam <timothy.lam@creatogether.app>
2024-11-01 04:52:17 -0400

    remove tab in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useMemo, useState } from 'react'
+import { ReactElement, useCallback, useMemo, useState } from 'react'
 
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 import Link from 'next/link'
@@ -13,13 +13,13 @@ import ROUTE from '@constants/route'
 import { useKeyword, useMostRepeatedPost, useMostRepeatedPostImage, usePostCount, useProfile } from '@hooks/useMeta'
 import { getFilteredAccounts } from '@services/Account/Account'
 import { CountryEnum } from 'enums/CountryCodeEnums'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/Tabs'
 import Dropdown from '@components/Form/Dropdown/Dropdown'
 import SideMenuLayout from '@components/Layout/SideMenuLayout'
 import { Layout } from '@components/Layout'
 import { getRoles } from '@services/util'
 import { DatePickerWithRange } from '@components/ui/DatePickerWithRange'
 import { DateRange } from 'react-day-picker'
+import NavigationPill from '@components/ui/NavigationPill'
 
 type Props = {
   botList: IAccount[]
@@ -95,6 +95,8 @@ const Dashboard = ({ botList }: Props) => {
     from: new Date(new Date().setDate(new Date().getDate() - 7)), // 7 days ago
     to: new Date(), // today
   })
+
+  const [reportDateRange, setReportDateRange] = useState(3)
 
   const { data: keywordData, isLoading: keywordIsLoading } = useKeyword(metaAttributes)
   const { data: postCountData, isLoading: postCountIsLoading } = usePostCount(metaAttributes)
@@ -186,70 +188,126 @@ const Dashboard = ({ botList }: Props) => {
     })
   }, [botList])
 
+  const MonthGroup = () => {
+    const reportType = [
+      {
+        name: '3 Days Report',
+        value: 3,
+      },
+      {
+        name: '7 Days Report',
+        value: 7,
+      },
+      {
+        name: 'Customized Period',
+        value: 0,
+      },
+    ]
+    const onSelect = useCallback((value: string | number) => {
+      setReportDateRange(value as number)
+    }, [])
+
+    return (
+      <div className="flex flex-row flex-wrap gap-2">
+        {reportType.map((key) => {
+          return <NavigationPill key={key.name} name={key.name} value={key.value} onSelect={onSelect} selected={key.value === reportDateRange} />
+        })}
+      </div>
+    )
+  }
+
+  const CustomPeriod = () => {
+    return (
+      <div className="flex h-full w-full flex-col justify-center gap-2">
+        <div className="text-neutral-800">Select Date-to-Dae</div>
+        <DatePickerWithRange setDate={setDate} date={date} className="w-full" />
+        <div className="text-neutral-500">Minimum 3 days need to be picked</div>
+      </div>
+    )
+  }
   return (
     <div className="flex w-full justify-center ">
-      <div className="flex w-full max-w-screen-xl flex-col">
+      <div className="flex w-full max-w-screen-xl flex-col pb-12">
         <div className="flex w-full flex-col">
           <h1 className="text-heading">Instagram Trend Analysis</h1>
-          {instaBotList && (
-            <div>
-              <div className="relative flex flex-col gap-2 px-4 py-6">
-                Instabot Account
-                <div className="flex w-64 flex-row items-center gap-6">
-                  <Dropdown
-                    className="flex"
-                    onValueChange={(e) => onAccountChange(e)}
-                    value={selectedAccount?.id}
-                    defaultValue={selectedAccount?.id}
-                    options={instaBotList}
-                    name={instaBotList[0].label}
-                    dropDownSizes={['s', 's', 's']}
-                    isFloating
-                  />
-                  <Link href={profile?.data.url ? profile.data.url : ''} target="_blank" className="flex min-h-6 min-w-6">
-                    <Image className="cursor-pointer" alt={'account share button'} src={'./external-link.svg'} width={32} height={32} />
-                  </Link>
+
+          <div className="">
+            <div className="mt-4">
+              <MonthGroup />
+              <div className="mx-4 border-b border-neutral-300 pt-4"></div>
+            </div>
+            {instaBotList && (
+              <div
+                className={`mt-4 flex flex-col-reverse gap-4 px-4 md:py-6 ${
+                  reportDateRange === 0 && 'rounded-bl-lg rounded-br-lg border-b border-l border-r py-4 shadow-lg'
+                } items-start md:flex-row`}
+              >
+                {reportDateRange === 0 && (
+                  <div className="w-1/2">
+                    <CustomPeriod />
+                  </div>
+                )}
+                <div className="flex w-full flex-col justify-center gap-2 ">
+                  Instabot Account
+                  <div className="flex flex-row gap-2">
+                    <div className="flex w-4/5 md:w-3/5">
+                      <Dropdown
+                        onValueChange={(e) => onAccountChange(e)}
+                        value={selectedAccount?.id}
+                        defaultValue={selectedAccount?.id}
+                        options={instaBotList}
+                        name={instaBotList[0].label}
+                        dropDownSizes={['s', 's', 's']}
+                        isFloating
+                      />
+                    </div>
+                    <div>
+                      <Link href={profile?.data.url ? profile.data.url : ''} target="_blank" className="relative flex h-full w-full">
+                        <Image
+                          className="cursor-pointer"
+                          objectFit="contain"
+                          alt={'account share button'}
+                          src={'./external-link.svg'}
+                          width={32}
+                          height={32}
+                        />
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="">
-                <DatePickerWithRange date={date} className={''} setDate={setDate} />
-              </div>
-              <div className="mx-4 border-b border-neutral-300 pt-4"></div>
+            )}
+          </div>
+          {!botList || botList.length == 0 ? (
+            <div className="flex w-full flex-col items-center justify-center gap-4 px-4 py-4 md:py-24 md:pt-12">
+              <img alt="missing insta bot" className="h-auto w-96" src={'/no-insta-bot.png'} />
+              <h2 className="font-extrabold">You have no linked instabot</h2>
+              <h3 className="items-center text-center text-text-secondary">
+                Your account does not have any verified instabot. Complete the adding account process to see dashboard.
+              </h3>
+              <Link href={ROUTE.ACCOUNT_BOT_CREATE}>
+                <Primary sizes={['s', 'l', 'l']} className="px-2">
+                  <div className="flex flex-row items-center gap-2">
+                    <PlusIcon className="h-6 w-6" />
+                    <div className="">Add New Account</div>
+                  </div>
+                </Primary>
+              </Link>
+            </div>
+          ) : (
+            <div className="gap-x-64s mx-4 mt-6 flex flex-row overflow-x-auto">
+              {tabItems.map((item) => (
+                <div key={item.key} className="flex-1">
+                  {item.children}
+                </div>
+              ))}
             </div>
           )}
         </div>
-        {!botList || botList.length == 0 ? (
-          <div className="flex w-full flex-col items-center justify-center gap-4 px-4 py-4 md:py-24 md:pt-12">
-            <img alt="missing insta bot" className="h-auto w-96" src={'/no-insta-bot.png'} />
-            <h2 className="font-extrabold">You have no linked instabot</h2>
-            <h3 className="items-center text-center text-text-secondary">
-              Your account does not have any verified instabot. Complete the adding account process to see dashboard.
-            </h3>
-            <Link href={ROUTE.ACCOUNT_BOT_CREATE}>
-              <Primary sizes={['s', 'l', 'l']} className="px-2">
-                <div className="flex flex-row items-center gap-2">
-                  <PlusIcon className="h-6 w-6" />
-                  <div className="">Add New Account</div>
-                </div>
-              </Primary>
-            </Link>
-          </div>
-        ) : (
-          <Tabs defaultValue={tabItems[0].value} className="mt-4 w-full ">
-            <TabsList>
-              {tabItems.map((item) => (
-                <TabsTrigger className="w-full" key={item.key} value={item.value}>
-                  {item.title}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-            {tabItems.map((item) => (
-              <TabsContent key={item.key} value={item.value}>
-                {item.children}
-              </TabsContent>
-            ))}
-          </Tabs>
-        )}
+        <caption className="px-4 text-start text-sm text-neutral-500">
+          Please note: This tool may display offensive material that doesn&apos;t represent 2 Tag&apos;s views. You&apos;re solely responsible for use
+          of any content generated using this tool, including its compliance with applicable laws and third-party rights.
+        </caption>
       </div>
     </div>
   )
