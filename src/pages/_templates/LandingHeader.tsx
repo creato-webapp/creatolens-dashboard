38ee7ee9f21a41a824fd12e1ab2c135a94dd12a8
Timothy.Lam <timothy.lam@creatogether.app>
2024-11-04 23:48:00 -0500

    add gif

--- src/pages/_templates/LandingHeader.tsx
+++ src/pages/_templates/LandingHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image'
 import PrimaryButton from '@components/Button/Primary'
 
 interface LandingHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
@@ -7,11 +7,12 @@ interface LandingHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 export default function LandingHeader(props: LandingHeaderProps) {
   const TransparentCard = () => {
     return (
-      <div className="flex w-full flex-col items-center gap-2 rounded-3xl bg-white px-2 py-6 opacity-90 md:px-8">
-        <h1 className="text-title font-bold">The magical tool that gets engagement</h1>
-        <h2 className="text-subtitle font-normal">Keyword, SEO Insight, Gen AI All in one</h2>
+      <div className="flex w-full flex-col items-center gap-2 rounded-3xl bg-white bg-opacity-60 py-6 shadow-lg shadow-primary-200 drop-shadow-lg md:px-8">
+        <h1 className="text-title font-bold">The magical tool that gets</h1>
+        <h1 className="w-full bg-primary-500 text-title font-bold text-white  md:text-title">ENGAGEMENT</h1>
+        <h1 className="text-subtitle font-normal">All in one</h1>
         <div className="flex flex-col items-center gap-8">
-          <PrimaryButton className="drop-shadow-md">Start Exploring</PrimaryButton>
+          <PrimaryButton className=" drop-shadow-md">Start Exploring</PrimaryButton>
           <div className="text-xs text-neutral-500">Start with $0. No credit card needed.</div>
           <div className="text-xs text-neutral-500">7 days a week customer services</div>
         </div>
@@ -20,10 +21,13 @@ export default function LandingHeader(props: LandingHeaderProps) {
   }
 
   return (
-    <div className={`${props.className} w-full bg-cover bg-bottom bg-no-repeat md:bg-[url('/landing-background2.png')]`}>
-      <div className="flex h-[calc(100vh-95px)] items-center justify-center text-center">
-        <div className="md:w-1/2 lg:w-1/3">
+    <div className={`${props.className} w-full`}>
+      <div className="relative z-10 flex h-full flex-col-reverse items-center justify-center py-16 text-center md:h-[calc(100vh-95px)] md:flex-row">
+        <div className="z-10 w-full md:w-1/2 md:px-4 lg:w-1/3">
           <TransparentCard />
+        </div>
+        <div className="absolute left-0 top-0 h-full w-full opacity-15 md:relative md:w-2/5 md:opacity-100">
+          <Image src="/landing-animation.gif" objectFit="coverImage" alt={'2Tag animation'} fill />
         </div>
       </div>
     </div>
