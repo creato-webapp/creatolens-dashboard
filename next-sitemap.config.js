51adad08bad753448275c697ff33e524c9c89c82
Timothy.Lam <timothy.lam@creatogether.app>
2024-11-29 00:01:41 -0500

    create sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://2tag.ai',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
