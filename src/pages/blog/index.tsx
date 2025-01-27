222b4f4b99fe9ca11d4a3c044be180d570455cd0
timothy.lam <timothy.lam@creatogether.app>
2025-01-27 11:27:45 -0500

    fix style prob

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -115,10 +115,11 @@ const Blog = ({ data }: BlogListProps) => {
                                 {index < item.tags.length - 1 && <span className="text-sm text-gray-500">/</span>}
                               </>
                             ))}
+                          ‰
                         </div>
                       </div>
                       <div className="relative aspect-[4/3] w-full overflow-hidden rounded-lg md:w-1/2 md:max-w-72">
-                        {item.featuredImage && <BlurredImage src={transformWixImageURL(item.featuredImage)} alt={item.title} fallbackSrc={IMAGE.LOGO_2TAG} />}
+                        <BlurredImage src={transformWixImageURL(item.featuredImage)} alt={item.title} fallbackSrc={IMAGE.LOGO_2TAG} />
                       </div>
                     </div>
                   </Link>
