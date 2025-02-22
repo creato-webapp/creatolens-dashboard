51eef4fe8ced231b4f585ab04b95497906e7c8e9
creato-webapp <webapp@creatogether.app>
2025-02-23 03:35:05 +0800

    Dev/web3 hackathon (#174)
    * web3 endpoint
    
    * endpoint
    
    ---------
    
    Co-authored-by: kalong-leung <leonellus0123@gmail.com>

---
+++ src/pages/api/web3/image.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import handler from '@helpers/api/handlers'
+import METHOD from '@constants/method'
+import { ImageInstance } from '@helpers/axios'
+
+type PromptData = {
+  input: string
+  key: string
+  aspectRatio: string
+  negativeKeywords: string
+}
+
+export default handler.api({
+  [METHOD.POST]: async (req: NextApiRequest, res: NextApiResponse) => {
+    const body = req.body as PromptData
+
+    const response = await ImageInstance.post<string>(`/api/metamove/image/prompt`, {
+      prompt: body.input,
+      user_id: `web3/${body.key}`,
+      aspect_ratio: body.aspectRatio,
+      negative_prompt: body.negativeKeywords,
+    })
+
+    return res.status(200).json(response.data)
+  },
+})
