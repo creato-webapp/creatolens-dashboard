26f1e4cf1665b44bec18d3fd33f3b47bac8caafb
timothylam-creato <152890351+timothylam-creato@users.noreply.github.com>
2025-03-03 18:57:07 -0500

    add ifrm (#177)

---
+++ src/pages/web3/aptos.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AptosPage = () => {
+  return (
+    <div className="h-screen w-full">
+      <iframe
+        src="https://langchain-nextjs-template-five-wheat.vercel.app/"
+        className="h-full w-full border-0"
+        title="LangChain NextJS Template"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
+
+export default AptosPage
